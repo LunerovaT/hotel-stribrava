3d736de57f388fd3e52d50ac0c85a8e089de54f3
Tereza Lunerova <lunerova.tereza@seznam.cz>
2025-05-24 12:01:10 +0200

    oprava komponenty

--- my-app/src/pages/HomePage/index.jsx
+++ my-app/src/pages/HomePage/index.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import Banner from '../components/Banner';
-import IntroSection from '../components/IntroSection';
-import CardSection from '../components/CardSection';
-import FormSection from '../components/FormSection';
-import InfoSection from '../components/InfoSection';
+import Banner from '../../components/Banner/Banner';
+import CardSection from '../../components/CardSection/CardSection';
+import FormSection from '../../components/FormSection/FormSection';
+import InfoSection from '../../components/InfoSection/InfoSection';
+import './style.css';
 
 export default function HomePage() {
   return (
     <>
       <Banner />
-      <IntroSection />
       <CardSection />
       <FormSection />
       <InfoSection />
